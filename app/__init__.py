fbd38250147752f3c2349eae3c1cd732f892edf8
Ryan Zhou <144398428+RyanThe1o8th@users.noreply.github.com>
2024-12-05 15:58:18 +0000

    finished routing

--- app/__init__.py
+++ app/__init__.py
@@ -26,13 +26,36 @@ def register():
     return render_template("register.html")
 
 @app.route('/view')
+def view():
+    return render_template("view.html")
+
 @app.route('/search')
+def search():
+    return render_template("search.html")
+
 @app.route('/create')
+def create():
+    return render_template("create.html")
+
 @app.route('/edit')
+def edit():
+    return render_template("edit.html")
+
 @app.route('/calender')
+def calender():
+    return render_template("calender.html")
+
 @app.route('/hrecipes')
+def hrecipes():
+    return render_template("hrecipes.html")
+
 @app.route('/settings')
+def settings():
+    return render_template("settings.html")
+
 @app.route('/logout')
+def logout():
+    return redirect(url_for('dashboard'))
 
 
 if __name__ == "__main__":
