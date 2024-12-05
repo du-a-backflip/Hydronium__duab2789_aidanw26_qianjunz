68b397291feb808e45b393ff438806b8a50cf93c
awong50 <144462013+awong50@users.noreply.github.com>
2024-12-05 15:33:14 +0000

    Uploaded working flask app

--- app/__init__.py
+++ app/__init__.py
@@ -7,9 +7,17 @@ Time Spent: 2
 """
 
 from flask import Flask, render_template, request, redirect, url_for, session, flash
-import sqlite3
+import sqlite3, os
+
+app = Flask(__name__)
+
+app.secret_key = os.urandom(32)
 
 @app.route('/')
+def home():
+    return "Welcome to the home page"
+
+"""
 @app.route('/login')
 @app.route('/register')
 @app.route('/view')
@@ -19,3 +27,8 @@ import sqlite3
 @app.route('/calender')
 @app.route('/hrecipes')
 @app.route('/settings')
+"""
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
